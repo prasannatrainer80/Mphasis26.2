ba2dcfa0034639cd229ad625d4a9d662209da1d1
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-02-17 11:32:20 +0530

    coe

---
+++ Day4/EmployDao.java
@@ -0,0 +1,14 @@
+package com.java.jsp.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.java.jsp.model.Employ;
+
+public interface EmployDao {
+	List<Employ> showEmploy() throws ClassNotFoundException, SQLException;
+	Employ searchEmploy(int empno) throws ClassNotFoundException, SQLException;
+	String addEmploy(Employ employ) throws ClassNotFoundException, SQLException;
+	String deleteEmploy(int empno) throws ClassNotFoundException, SQLException;
+	String updateEmploy(Employ employ) throws ClassNotFoundException, SQLException;
+}	
---
+++ Day4/EmployDaoImpl.java
@@ -0,0 +1,102 @@
+package com.java.jsp.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.java.jsp.model.Employ;
+import com.java.jsp.model.Gender;
+import com.java.jsp.util.ConnectionHelper;
+
+public class EmployDaoImpl implements EmployDao {
+
+	Connection connection;
+	PreparedStatement psmt;
+	
+	@Override
+	public List<Employ> showEmploy() throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select * from Employ";
+		psmt = connection.prepareStatement(cmd);
+		ResultSet rs = psmt.executeQuery();
+		List<Employ> employList = new ArrayList<Employ>();
+		Employ employ = null;
+		while(rs.next()) {
+			employ = new Employ();
+			employ.setEmpno(rs.getInt("empno"));
+			employ.setName(rs.getString("name"));
+			employ.setGender(Gender.valueOf(rs.getString("gender")));
+			employ.setDept(rs.getString("dept"));
+			employ.setDesig(rs.getString("desig"));
+			employ.setBasic(rs.getDouble("basic"));
+			employList.add(employ);
+		}
+		return employList;
+	}
+
+	@Override
+	public Employ searchEmploy(int empno) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select * from Employ where empno = ?";
+		psmt = connection.prepareStatement(cmd);
+		psmt.setInt(1, empno);
+		ResultSet rs = psmt.executeQuery();
+		Employ employ = null;
+		if (rs.next()) {
+			employ = new Employ();
+			employ.setEmpno(rs.getInt("empno"));
+			employ.setName(rs.getString("name"));
+			employ.setGender(Gender.valueOf(rs.getString("gender")));
+			employ.setDept(rs.getString("dept"));
+			employ.setDesig(rs.getString("desig"));
+			employ.setBasic(rs.getDouble("basic"));
+		}
+		return employ;
+	}
+
+	@Override
+	public String addEmploy(Employ employ) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "Insert into Employ(empno,name,gender,dept,desig,basic)  "
+				+ " values(?,?,?,?,?,?)";
+		psmt = connection.prepareStatement(cmd);
+		psmt.setInt(1, employ.getEmpno());
+		psmt.setString(2, employ.getName());
+		psmt.setString(3, employ.getGender().toString());
+		psmt.setString(4, employ.getDept());
+		psmt.setString(5, employ.getDesig());
+		psmt.setDouble(6, employ.getBasic());
+		psmt.executeUpdate();
+		return "Employ Record Inserted...";
+	}
+
+	@Override
+	public String deleteEmploy(int empno) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "delete from Employ where empno = ?";
+		psmt = connection.prepareStatement(cmd);
+		psmt.setInt(1, empno);
+		psmt.executeUpdate();
+		return "Employ Record Deleted...";
+	}
+
+	@Override
+	public String updateEmploy(Employ employ) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "Update Employ set Name = ?, Gender = ?, Dept = ?, "
+				+ " Desig = ?, Basic = ? Where Empno = ?";
+		psmt = connection.prepareStatement(cmd);
+		psmt.setString(1, employ.getName());
+		psmt.setString(2, employ.getGender().toString());
+		psmt.setString(3, employ.getDept());
+		psmt.setString(4, employ.getDesig());
+		psmt.setDouble(5, employ.getBasic());
+		psmt.setInt(6, employ.getEmpno());
+		psmt.executeUpdate();
+		return "Employ Record Updated...";
+	}
+
+}
