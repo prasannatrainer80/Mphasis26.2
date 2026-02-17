7ec25619aae9f68bbe8c8af5d11b7a7b1b95f9d1
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-02-17 17:16:10 +0530

    Cdoe

---
+++ Day4/Agent.java
@@ -0,0 +1,12 @@
+package com.java.jsp;
+
+public class Agent {
+	private int agentId;
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String state;
+	private String country;
+	private String gender;
+	private double premiumAmount;
+}
