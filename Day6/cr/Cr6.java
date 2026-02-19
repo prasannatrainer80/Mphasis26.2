7095061e13928af070394f69492531c5ec867229
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-02-19 17:14:32 +0530

    code

---
+++ Day6/cr/Cr6.java
@@ -0,0 +1,25 @@
+package com.java.hib;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+
+import com.java.hib.model.Employ;
+import com.java.hib.util.SessionHelper;
+
+public class Cr6 {
+	public static void main(String[] args) {
+		SessionFactory sf = SessionHelper.getSession();
+		Session session = sf.openSession();
+		Criteria cr = session.createCriteria(Employ.class);
+		cr.add(Restrictions.in("dept", 
+				new String[] {"Java","Sql"}
+				));
+		List<Employ> employList = cr.list();
+		employList.forEach(System.out::println);
+
+	}
+}
---
+++ Day6/cr/GrpEx2.java
@@ -0,0 +1,29 @@
+package com.java.hib;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+
+import com.java.hib.model.Employ;
+import com.java.hib.util.SessionHelper;
+
+public class GrpEx2 {
+	public static void main(String[] args) {
+		SessionFactory sf = SessionHelper.getSession();
+		Session session = sf.openSession();
+		Criteria cr = session.createCriteria(Employ.class);
+		ProjectionList plist = Projections.projectionList();
+		plist.add(Projections.groupProperty("dept"));
+		plist.add(Projections.rowCount());
+		cr.setProjection(plist);
+		List<Object[]> list = cr.list();
+		for(Object[] row : list) {
+			System.out.println("Dept  " +row[0]);
+			System.out.println("Total Employees " +row[1]);
+		}
+	}
+}
