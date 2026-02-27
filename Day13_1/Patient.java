784f33f93fd16cfef3b748baf4fd6222912d23b5
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-02-27 10:47:36 +0530

    code

---
+++ Day13_1/Patient.java
@@ -0,0 +1,16 @@
+package com.java.mvc.model;
+
+public class Patient {
+
+	private int patientId;
+	private String patientName;
+	private String gender;
+	private double billAmount;
+	private String city;
+	private String state;
+	private String policyNumber;
+	private double claimAmount;
+	private String insuranceName;
+	private double pendingAmount;
+	
+}
