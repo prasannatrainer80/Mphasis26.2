d463b05120400db50b111f474b3a5b5ece91d9ad
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-02-27 14:42:15 +0530

    code

---
+++ Day13_2/HomeController.java
@@ -0,0 +1,48 @@
+package com.java.mvc.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.java.mvc.dao.EmployDao;
+import com.java.mvc.model.Employ;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private EmployDao employDao;
+
+	@RequestMapping(value="/loginresult")
+	public ModelAndView test(HttpServletRequest request) throws IOException
+	{
+		String user = request.getParameter("userName");
+		String pwd = request.getParameter("passCode");
+		int res = employDao.login(user, pwd);
+		if (res==1) {
+			return new ModelAndView("redirect:/showemploy");
+		}
+		return new ModelAndView("redirect:/");
+	}
+	
+	@RequestMapping(value="/showemploy")
+	public ModelAndView showEmploy() {
+		List<Employ> employList = employDao.showEmploy();
+		return new ModelAndView("home","employList",employList);
+	}
+	
+
+
+	
+	@RequestMapping(value="/")
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+		return new ModelAndView("login");
+	}
+}
