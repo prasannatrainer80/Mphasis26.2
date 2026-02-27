f17254c8a21e925e6fc59c383b04a758ef697a30
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-02-27 09:59:11 +0530

    code

---
+++ Day13_1/HomeController.java
@@ -0,0 +1,100 @@
+package com.java.mvc.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.java.mvc.model.Employ;
+
+@Controller
+public class HomeController {
+
+
+	private static List<Employ> employList = new ArrayList<>(Arrays.asList(
+		    new Employ(1, "Mythri", "FEMALE", "Java", "Programmer", 84824),
+		    new Employ(2, "Aakash", "MALE", "Java", "Expert", 90886),
+		    new Employ(3, "Shakeeb", "MALE", "Dotnet", "Programmer", 92824),
+		    new Employ(4, "Rishika", "FEMALE", "Java", "TL", 89022),
+		    new Employ(5, "Sravan", "MALE", "Sql", "Admin", 97222)
+		));
+
+	
+	@RequestMapping(value="/nameresult")
+	public ModelAndView nameRequest(HttpServletRequest request) {
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String fullName = firstName + " " +lastName;
+		ModelAndView mv = new ModelAndView("nameresult");
+		mv.addObject("firstName",firstName);
+		mv.addObject("lastName",lastName);
+		mv.addObject("fullName",fullName);
+		return mv;
+
+	}
+	
+	@RequestMapping(value="/shownames")
+	public ModelAndView show() {
+		List<String> names = new ArrayList<String>();
+		names.add("Srinivas");
+		names.add("Bala");
+		names.add("Kabila");
+		names.add("Arvind");
+		names.add("Satish");
+		names.add("Suganya");
+		return new ModelAndView("shownames","names",names);
+	}
+	@RequestMapping(value="/loginauth")
+	public ModelAndView auth(HttpServletRequest req) {
+		String user = req.getParameter("userName");
+		String pwd = req.getParameter("passCode");
+		if (user.equals("Mphasis") && pwd.equals("Mphasis")) {
+			return new ModelAndView("menu");
+		} else {
+			return new ModelAndView("login","error","Invalid Credentials...");
+		}
+	}
+	
+	@RequestMapping(value="/login")
+	public ModelAndView loginForm() {
+		return new ModelAndView("login");
+	}
+	
+	@RequestMapping(value="/demoform")
+	public ModelAndView demoForm() {
+		return new ModelAndView("demoform");
+	}
+	
+	@RequestMapping(value="/nameform")
+	public ModelAndView nameForm() {
+		return new ModelAndView("nameform");
+	}
+	
+	@RequestMapping(value="/demoshow")
+	public ModelAndView demoshow(HttpServletRequest req) {
+		String sname = req.getParameter("sname");
+		return new ModelAndView("demoshow","sname",sname);
+	}
+	@RequestMapping(value="/manoj")
+	public ModelAndView manoj() {
+		return new ModelAndView("manoj");
+	}
+	@RequestMapping(value="/arvind")
+	public ModelAndView arvind() {
+		return new ModelAndView("arvind");
+	}
+	
+	@RequestMapping(value="/")
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+		return new ModelAndView("home");
+	}
+}
