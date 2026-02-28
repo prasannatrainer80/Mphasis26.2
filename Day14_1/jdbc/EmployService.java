51d0b383d0f38450b5fe59e52522cfd0d17c0f86
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-02-28 14:54:53 +0530

    code

--- Day14_1/jdbc/EmployService.java
+++ Day14_1/jdbc/EmployService.java
@@ -2,8 +2,13 @@ package com.java.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -22,6 +27,37 @@ public class EmployService {
 		employDao = (EmployDaoImpl)context.getBean("employDaoImpl");
 	}
 
+	 @POST
+	 @Path("/addemploy/")
+	 @Consumes(MediaType.APPLICATION_JSON)
+	 @Produces(MediaType.APPLICATION_JSON)
+	 public String insertEmploy(final Employ e) {
+		  return employDao.addEmploy(e);
+	 }
+
+	  @DELETE
+	  @Path("/employdelete/{empno}")
+	  @Produces(MediaType.APPLICATION_JSON)
+	  public String deleteEmploy(@PathParam("empno") int empno) {
+		  return employDao.deleteEmploy(empno);
+	  }
+	
+	  @PUT
+	  @Path("/updateemploy/")
+	  @Consumes(MediaType.APPLICATION_JSON)
+	  @Produces(MediaType.APPLICATION_JSON)
+	  public String updateEmploy(final Employ e) {
+		  return employDao.updateEmploy(e);
+	  }
+
+	
+	@GET
+	@Path("/searchemploy/{empno}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Employ searchEmploy(@PathParam("empno") int empno) {
+		return employDao.searchEmploy(empno);
+	}
+	
 	@GET
 	@Path("/showemploy")
 	@Produces(MediaType.APPLICATION_JSON)
