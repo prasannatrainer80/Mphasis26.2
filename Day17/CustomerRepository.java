5a6c04245c63649cff721d1e7b153c849e012065
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-03-04 10:31:55 +0530

    code

---
+++ Day17/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+	Customer findByCusUserName(String cusUserName);
+	Customer findByCusUserNameAndCusPassword(String user, String pwd);
+
+}
---
+++ Day17/CustomerService.java
@@ -0,0 +1,39 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Customer;
+import com.example.demo.repo.CustomerRepository;
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	public String loginCustomer(String user, String pwd) {
+		Customer customer = customerRepository.findByCusUserNameAndCusPassword(user, pwd);
+		if (customer!=null) {
+			return "1";
+		}
+		return "0";
+	}
+	public Customer searchByuserName(String user) {
+		Customer customer = customerRepository.findByCusUserName(user);
+		System.out.println("Customer  " +customer);
+		return customer;
+		
+	}
+	
+	public List<Customer> showCustomer() {
+		return customerRepository.findAll();
+	}
+	
+	public Customer searchByCustomerId(int id) {
+		return customerRepository.findById(id).get();
+	}
+
+}
